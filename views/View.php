1f4e19058aa0df510aa242292d2c70cb9289bffe
Alan Santos <alanssantos32@gmail.com>
2020-11-19 14:58:58 -0300

    template for view

--- views/View.php
+++ views/View.php
@@ -3,8 +3,35 @@
     class View {
 
         protected $dir = 'views/templates/';
+        protected $baseTemplate = 'views/base.php';
+        protected $args = array(
+            'title' => 'Facil Consulta'
+        );
+
+        private function getBaseTemplate($arc) {
+            if (is_file($arc)) return file_get_contents($arc);
+        }
+
+        private function getTemplate($arc) {
+            if (is_file($arc)) return file_get_contents($arc);
+        }
+
+        private function parseTemplate($base, $array) {
+            foreach ($array as $a => $b) {
+                $base = str_replace(
+                    array(
+                        '{{'.$a.'}}', '{{ '.$a.' }}'
+                    ), $b, $base
+                );
+            }
+
+            return $base;
+        }
 
         public function home() {
-            include $this->dir . 'home.php';
+            $base = $this->getBaseTemplate($this->baseTemplate);
+            $this->args['template'] = $this->getTemplate($this->dir . 'home.php');
+            
+            echo $this->parseTemplate($base, $this->args);
         }
     }
